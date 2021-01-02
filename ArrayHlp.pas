0a38106328961a2cd36463254e0efcf3a8d25923
Jaap Baak <baak@significance.nl>
2021-01-02 12:03:40 +0100

    Added (alternative) record helpers for float64 and dynamic int/float arrays

---
+++ ArrayHlp.pas
@@ -0,0 +1,142 @@
+unit ArrayHlp;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Author: Jaap Baak
+// https://github.com/transportmodelling/Utils
+//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+interface
+////////////////////////////////////////////////////////////////////////////////
+
+Uses
+  Math;
+
+Type
+  TIntArrayHelper = record helper for TArray<Integer>
+  private
+    Function GetLength: Integer; inline;
+    Procedure SetLength(Length: Integer); inline;
+  public
+    Procedure Initialize(Value: Integer);
+    Procedure Assign(const Values: array of Integer);
+    Procedure Append(const Values: array of Integer);
+    Function  MinValue: Integer;
+    Function  MaxValue: Integer;
+    Function  Total: Integer;
+  public
+    Property Length: Integer read GetLength write SetLength;
+  end;
+
+  TFloat64ArrayHelper = record helper for TArray<Float64>
+  private
+    Function GetLength: Integer; inline;
+    Procedure SetLength(Length: Integer); inline;
+  public
+    Procedure Initialize(Value: Float64);
+    Procedure Assign(const Values: array of Float64);
+    Procedure Append(const Values: array of Float64);
+    Function  MinValue: Float64;
+    Function  MaxValue: Float64;
+    Function  Total: Float64;
+  public
+    Property Length: Integer read GetLength write SetLength;
+  end;
+
+////////////////////////////////////////////////////////////////////////////////
+implementation
+////////////////////////////////////////////////////////////////////////////////
+
+Function TIntArrayHelper.GetLength: Integer;
+begin
+  Result := System.Length(Self);
+end;
+
+Procedure TIntArrayHelper.SetLength(Length: Integer);
+begin
+  System.SetLength(Self,Length);
+end;
+
+Procedure TIntArrayHelper.Initialize(Value: Integer);
+begin
+  for var Index := 0 to Length-1 do Self[Index] := Value;
+end;
+
+Procedure TIntArrayHelper.Assign(const Values: array of Integer);
+begin
+  Length := System.Length(Values);
+  for var Index := 0 to Length-1 do Self[Index] := Values[Index];
+end;
+
+Procedure TIntArrayHelper.Append(const Values: array of Integer);
+begin
+  var Offset := Length;
+  Length := Offset + System.Length(Values);
+  for var Index := Offset to Length-1 do Self[Index] := Values[Index-Offset];
+end;
+
+Function TIntArrayHelper.MinValue: Integer;
+begin
+  Result := MinIntValue(Self);
+end;
+
+Function TIntArrayHelper.MaxValue: Integer;
+begin
+  Result := MaxIntValue(Self);
+end;
+
+Function TIntArrayHelper.Total: Integer;
+begin
+  Result := 0;
+  for var Index := 0 to Length-1 do Result := Result  + Self[Index];
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+Function TFloat64ArrayHelper.GetLength: Integer;
+begin
+  Result := System.Length(Self);
+end;
+
+Procedure TFloat64ArrayHelper.SetLength(Length: Integer);
+begin
+  System.SetLength(Self,Length);
+end;
+
+Procedure TFloat64ArrayHelper.Initialize(Value: Float64);
+begin
+  for var Index := 0 to Length-1 do Self[Index] := Value;
+end;
+
+Procedure TFloat64ArrayHelper.Assign(const Values: array of Float64);
+begin
+  Length := System.Length(Values);
+  for var Index := 0 to Length-1 do Self[Index] := Values[Index];
+end;
+
+Procedure TFloat64ArrayHelper.Append(const Values: array of Float64);
+begin
+  var Offset := Length;
+  Length := Offset + System.Length(Values);
+  for var Index := Offset to Length-1 do Self[Index] := Values[Index-Offset];
+end;
+
+Function TFloat64ArrayHelper.MinValue: Float64;
+begin
+  Result := Math.MinValue(Self);
+end;
+
+Function TFloat64ArrayHelper.MaxValue: Float64;
+begin
+  Result := Math.MaxValue(Self);
+end;
+
+Function TFloat64ArrayHelper.Total: Float64;
+begin
+  Result := 0.0;
+  for var Index := 0 to Length-1 do Result := Result  + Self[Index];
+end;
+
+end.
---
+++ FloatHlp.pas
@@ -0,0 +1,56 @@
+unit FloatHlp;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Author: Jaap Baak
+// https://github.com/transportmodelling/Utils
+//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+interface
+////////////////////////////////////////////////////////////////////////////////
+
+Uses
+  SysUtils;
+
+Type
+  TFloat64Helper = record helper for Float64
+  public
+    Function Round: Int64;
+    Procedure Add(const Value: Float64);
+    Procedure MultiplyBy(const Value: Float64);
+    Function ToString: String; overload;
+    Function ToString(const Format: String): String; overload;
+  end;
+
+////////////////////////////////////////////////////////////////////////////////
+implementation
+////////////////////////////////////////////////////////////////////////////////
+
+Function TFloat64Helper.Round: Int64;
+begin
+  Result := System.Round(Self);
+end;
+
+Procedure TFloat64Helper.Add(const Value: Float64);
+begin
+  Self := Self + Value;
+end;
+
+Procedure TFloat64Helper.MultiplyBy(const Value: Float64);
+begin
+  Self := Value*Self;
+end;
+
+Function TFloat64Helper.ToString: String;
+begin
+  Result := FloatToStr(Self);
+end;
+
+Function TFloat64Helper.ToString(const Format: String): String;
+begin
+  Result := FormatFloat(Format,Self);
+end;
+
+end.
