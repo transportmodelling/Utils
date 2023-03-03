18c4343f23948c9c92fc1c463b639998e70942b0
Jaap Baak <baak@significance.nl>
2023-03-03 10:46:23 +0100

    Added TArrayView for read only array access

--- ArrayVal.pas
+++ ArrayVal.pas
@@ -15,6 +15,18 @@ Uses
   SysUtils;
 
 Type
+  TArrayView<T> = record
+  // Record that provides access to the array values, but prevents the array from being changed.
+  private
+    FValues: TArray<T>;
+    Function GetValues(Index: Integer): T; inline;
+  public
+    Constructor Create(Values: TArray<T>);
+    Function Length: Integer; inline;
+  public
+    Property Values[Index: Integer]: T read GetValues; default;
+  end;
+
   TArrayValues<T> = record
   // Record that provides access to the array values, but prevents the array from being resized.
   private
@@ -28,6 +40,11 @@ Type
     Property Values[Index: Integer]: T read GetValues write SetValues; default;
   end;
 
+  TStringArrayView = TArrayView<String>;
+  TIntArrayView = TArrayView<Integer>;
+  TFloat64ArrayView = TArrayView<Float64>;
+  TFloat32ArrayView = TArrayView<Float32>;
+
   TStringArrayValues = TArrayValues<String>;
   TIntArrayValues = TArrayValues<Integer>;
   TFloat64ArrayValues = TArrayValues<Float64>;
@@ -35,6 +52,23 @@ Type
 
 ////////////////////////////////////////////////////////////////////////////////
 implementation
+////////////////////////////////////////////////////////////////////////////////
+
+Constructor TArrayView<T>.Create(Values: TArray<T>);
+begin
+  FValues := Values;
+end;
+
+Function TArrayView<T>.GetValues(Index: Integer): T;
+begin
+  Result := FValues[Index];
+end;
+
+Function TArrayView<T>.Length: Integer;
+begin
+  Result := System.Length(FValues);
+end;
+
 ////////////////////////////////////////////////////////////////////////////////
 
 Constructor TArrayValues<T>.Create(Values: TArray<T>);
