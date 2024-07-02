0ec0cdd795def4beda99103df730c167133eb70f
Jaap Baak <baak@significance.nl>
2024-07-02 13:51:17 +0200

    Replace record by class

--- MemDBF.pas
+++ MemDBF.pas
@@ -2,6 +2,13 @@ unit MemDBF;
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+// Class to manipulate a dbf file. Data are read into a FireDac memory table to be
+// manipulated. The manipulated table can be saved to file again.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
 // Author: Jaap Baak
 // https://github.com/transportmodelling/Utils
 //
@@ -12,47 +19,113 @@ interface
 ////////////////////////////////////////////////////////////////////////////////
 
 Uses
-  SysUtils,Data.DB,FireDac.Comp.Client,DBF;
+  SysUtils, Data.DB, FireDac.Comp.Client, DBF;
 
 Type
-  TDBFMemDataSet = record
-    Class Procedure Read(const DBFFileName: String; const MemDataSet: TFDMemTable); static;
+  TMemDBF = Class
+  private
+    FFileName: String;
+    FTable: TFDMemTable;
+    Fields: TArray<TDBFField>;
+    OwnsTable: Boolean;
+  public
+    Constructor Create(const FileName: String); overload;
+    Constructor Create(const FileName: String; const Table: TFDMemTable); overload;
+    Procedure Save;
+    Procedure SaveAs(const FileName: String);
+    Destructor Destroy; override;
+  public
+    Property FileName: String read FFileName;
+    Property Table: TFDMemTable read FTable;
   end;
 
 ////////////////////////////////////////////////////////////////////////////////
 implementation
 ////////////////////////////////////////////////////////////////////////////////
 
-Class Procedure TDBFMemDataSet.Read(const DBFFileName: String; const MemDataSet: TFDMemTable);
+Constructor TMemDBF.Create(const FileName: String);
 begin
-  var DBFReader := TDBFReader.Create(DBFFileName);
-  try
-    // Read fields
-    MemDataSet.FieldDefs.Clear;
-    for var Field := 0 to DBFReader.FieldCount-1 do
-    case DBFReader.FieldTypes[Field] of
-      'C': MemDataSet.FieldDefs.Add(DBFReader.FieldNames[Field],ftString,DBFReader.FieldLength[Field]);
-      'D': MemDataSet.FieldDefs.Add(DBFReader.FieldNames[Field],ftDate);
-      'L': MemDataSet.FieldDefs.Add(DBFReader.FieldNames[Field],ftBoolean);
-      'F','N':
-           if DBFReader.DecimalCount[Field] = 0 then
-             MemDataSet.FieldDefs.Add(DBFReader.FieldNames[Field],ftInteger)
-           else
-             MemDataSet.FieldDefs.Add(DBFReader.FieldNames[Field],ftFloat)
+  Create(FileName,TFDMemTable.Create(nil));
+  OwnsTable := true;
+end;
+
+Constructor TMemDBF.Create(const FileName: String; const Table: TFDMemTable);
+begin
+  inherited Create;
+  if FileExists(FileName) then
+  begin
+    // Set property values
+    FFileName := FileName;
+    FTable := Table;
+    // Read table
+    var DBFReader := TDBFReader.Create(FileName);
+    try
+      // Read fields
+      Fields := DBFReader.GetFields;
+      FTable.FieldDefs.Clear;
+      for var Field in Fields do
+      case Field.FieldType of
+        'C': FTable.FieldDefs.Add(Field.FieldName,ftString,Field.FieldLength);
+        'D': FTable.FieldDefs.Add(Field.FieldName,ftDate);
+        'L': FTable.FieldDefs.Add(Field.FieldName,ftBoolean);
+        'F','N':
+             if Field.DecimalCount = 0 then
+               FTable.FieldDefs.Add(Field.FieldName,ftInteger)
+             else
+               FTable.FieldDefs.Add(Field.FieldName,ftFloat)
+      end;
+      FTable.CreateDataSet;
+      // Read data
+      for var Rec := 0 to DBFReader.RecordCount-1 do
+      if DBFReader.NextRecord then
+      begin
+        FTable.Append;
+        for var Field := 0 to DBFReader.FieldCount-1 do FTable.Fields[Field].Value := DBFReader[Field];
+      end else
+        raise exception.Create('Error reading DBF-file');
+      FTable.First;
+    finally
+      DBFReader.Free;
     end;
-    MemDataSet.CreateDataSet;
-    // Read data
-    for var Rec := 0 to DBFReader.RecordCount-1 do
-    if DBFReader.NextRecord then
-    begin
-      MemDataSet.Append;
-      for var Field := 0 to DBFReader.FieldCount-1 do MemDataSet.Fields[Field].Value := DBFReader[Field];
-    end else
-      raise exception.Create('Error reading DBF-file');
-    MemDataSet.First;
+  end else
+    raise Exception.Create('DBF-file does not exist');
+end;
+
+Procedure TMemDBF.Save;
+begin
+  SaveAs(FFileName);
+end;
+
+Procedure TMemDBF.SaveAs(const FileName: String);
+begin
+  // Set property value
+  FFileName := FileName;
+  // Write dbf-file
+  var DBFWriter := TDBFWriter.Create(FFileName,Fields);
+  try
+    FTable.DisableControls;
+    try
+      // Save data
+      FTable.First;
+      while not FTable.Eof do
+      begin
+        for var Field := low(Fields) to high(Fields) do
+        DBFWriter.FieldValues[Field] := FTable.Fields[Field].Value;
+        DBFWriter.AppendRecord;
+        FTable.Next;
+      end;
+    finally
+      FTable.EnableControls;
+    end;
   finally
-    DBFReader.Free;
+    DBFWriter.Free;
   end;
+end;
+
+Destructor TMemDBF.Destroy;
+begin
+  if OwnsTable then FTable.Free;
+  inherited Destroy;
 end;
 
 end.
