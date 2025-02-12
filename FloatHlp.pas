870c98225e83a4ae702fa4dc90c55f890c95334c
Jaap Baak <baak@significance.nl>
2025-02-12 19:23:12 +0100

    Add MultipliedBy and DividedBy functions

--- FloatHlp.pas
+++ FloatHlp.pas
@@ -21,7 +21,9 @@ Type
     Procedure Add(const Value: Float64);
     Procedure Subtract(const Value: Float64);
     Procedure MultiplyBy(const Value: Float64);
+    Function MultipliedBy(const Value: Float64): Float64;
     Procedure DivideBy(const Value: Float64);
+    Function DividedBy(const Value: Float64): Float64;
     Function ToString: String; overload;
     Function ToString(const Format: String): String; overload;
   end;
@@ -50,9 +52,19 @@ begin
   Self := Value*Self;
 end;
 
+Function TFloat64Helper.MultipliedBy(const Value: Float64): Float64;
+begin
+  Result := Value*Self;
+end;
+
 Procedure TFloat64Helper.DivideBy(const Value: Float64);
 begin
   Self := Self/Value;
+end;
+
+Function TFloat64Helper.DividedBy(const Value: Float64): Float64;
+begin
+  Result := Self/Value;
 end;
 
 Function TFloat64Helper.ToString: String;
