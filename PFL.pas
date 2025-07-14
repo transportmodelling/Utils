f7b17a89c7ff8d5f613d861255a672358a704b57
Jaap Baak <baak@significance.nl>
2025-07-14 15:16:16 +0200

    Re-use threads

--- PFL.pas
+++ PFL.pas
@@ -2,6 +2,31 @@ unit PFL;
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+// Unlike the PPL, the parallel for loop implemented in this unit provides the
+// index of the thread in the thread pool for each iteration. This allows to avoid intensive
+// use of TInterlocked.
+//
+// For example, the PPL code:
+//
+// TParallel.For(2,1,Max,procedure(I:Int64)
+//                       begin
+//                         if IsPrime(I) then TInterlocked.Increment(NPrimes);
+//                       end);
+//
+// Can be implemented using a ParallelFor-object of type TParallelFor:
+//
+// ParallelFor.Execute(1,Max,procedure(I,Thread:Integer)
+//                           begin
+//                             if IsPrime(I) then Inc(NPrimes[Thread]);
+//                           end, 2);
+//
+// and finally summing the NPrimes-array over all threads.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
 // Author: Jaap Baak
 // https://github.com/transportmodelling/Utils
 //
@@ -12,7 +37,7 @@ interface
 ////////////////////////////////////////////////////////////////////////////////
 
 Uses
-  Classes,SysUtils,SyncObjs;
+  Classes, SysUtils, Math, SyncObjs;
 
 Type
   TLoopIteration = reference to Procedure(Iteration,Thread: Integer);
@@ -22,31 +47,50 @@ Type
     Type
       TIterationThread = Class(TThread)
       private
+        Active: TEvent;
         Loop: TParallelFor;
-        Thread,Iteration: Integer;
+        Thread,Iteration,Stride: Integer;
         LoopIteration: TLoopIteration;
       public
+        Constructor Create;
         Procedure Execute; override;
+        Destructor Destroy; override;
       end;
     Var
+      FMaxThreads: Integer;
       LoopCompleted: TEvent;
       FirstException: Exception;
-      Next,IterationCount,ThreadCount: Integer;
+      Next,IterationCount,ActiveThreads: Integer;
+      ThreadPool: array of TIterationThread;
   public
-    Constructor Create;
-    Procedure Execute(FromIteration,ToIteration: Integer; const Iteration: TLoopIteration); overload;
-    Procedure Execute(NThreads,FromIteration,ToIteration: Integer; const Iteration: TLoopIteration); overload;
+    Constructor Create(ThreadCount: Integer);
+    Procedure Execute(const FromIteration,ToIteration: Integer;
+                      const Iteration: TLoopIteration;
+                      const Stride: Integer = 1); overload;
+    Procedure Execute(const NThreads,FromIteration,ToIteration: Integer;
+                      const Iteration: TLoopIteration;
+                      const Stride: Integer = 1); overload;
     Destructor Destroy; override;
+  public
+    Property MaxThreads: Integer read FMaxThreads;
   end;
 
 ////////////////////////////////////////////////////////////////////////////////
 implementation
 ////////////////////////////////////////////////////////////////////////////////
 
+Constructor TParallelFor.TIterationThread.Create;
+begin
+  inherited Create(true);
+  Active := TEvent.Create(nil,true,false,'');
+end;
+
 Procedure TParallelFor.TIterationThread.Execute;
 begin
   repeat
+    Active.WaitFor(Infinite);
     // Execute iteration
+    if not Terminated then
     try
       LoopIteration(Iteration,Thread);
     except
@@ -54,8 +98,8 @@ begin
       begin
         TMonitor.Enter(Loop);
         try
-          Dec(Loop.ThreadCount);
-          if Loop.ThreadCount = 0 then Loop.LoopCompleted.SetEvent;
+          Dec(Loop.ActiveThreads);
+          if Loop.ActiveThreads = 0 then Loop.LoopCompleted.SetEvent;
           if Loop.FirstException = nil then Loop.FirstException := AcquireExceptionObject as Exception;
           Terminate;
         finally
@@ -63,7 +107,6 @@ begin
         end;
       end;
     end;
-    // Collect next iteration
     if not Terminated then
     begin
       TMonitor.Enter(Loop);
@@ -71,13 +114,13 @@ begin
         if (Loop.FirstException = nil) and (Loop.IterationCount > 0) then
         begin
           Iteration := Loop.Next;
-          Inc(Loop.Next);
-          Dec(Loop.IterationCount);
+          Inc(Loop.Next,Stride);
+          Dec(Loop.IterationCount,Stride);
         end else
         begin
-          Dec(Loop.ThreadCount);
-          if Loop.ThreadCount = 0 then Loop.LoopCompleted.SetEvent;
-          Terminate;
+          Active.ResetEvent;
+          Dec(Loop.ActiveThreads);
+          if Loop.ActiveThreads = 0 then Loop.LoopCompleted.SetEvent;
         end;
       finally
         TMonitor.Exit(Loop);
@@ -86,41 +129,59 @@ begin
   until Terminated;
 end;
 
+Destructor TParallelFor.TIterationThread.Destroy;
+begin
+  Active.Free;
+  inherited Destroy;
+end;
+
 ////////////////////////////////////////////////////////////////////////////////
 
-Constructor TParallelFor.Create;
+Constructor TParallelFor.Create(ThreadCount: Integer);
 begin
   inherited Create;
+  FMaxThreads := ThreadCount;
+  SetLength(ThreadPool,ThreadCount);
   LoopCompleted := TEvent.Create(nil,false,true,'');
+  for var Thread := 0 to ThreadCount-1 do
+  begin
+    ThreadPool[Thread] := TIterationThread.Create;
+    ThreadPool[Thread].Loop := Self;
+    ThreadPool[Thread].Thread := Thread;
+    ThreadPool[Thread].FreeOnTerminate := true;
+    ThreadPool[Thread].Start;
+  end;
 end;
 
-Procedure TParallelFor.Execute(FromIteration,ToIteration: Integer; const Iteration: TLoopIteration);
+Procedure TParallelFor.Execute(const FromIteration,ToIteration: Integer;
+                               const Iteration: TLoopIteration;
+                               const Stride: Integer = 1);
 begin
-  Execute(TThread.ProcessorCount,FromIteration,ToIteration,Iteration);
+  Execute(FMaxThreads,FromIteration,ToIteration,Iteration,Stride);
 end;
 
-Procedure TParallelFor.Execute(NThreads,FromIteration,ToIteration: Integer; const Iteration: TLoopIteration);
+Procedure TParallelFor.Execute(const NThreads,FromIteration,ToIteration: Integer;
+                               const Iteration: TLoopIteration;
+                               const Stride: Integer = 1);
 begin
   if ToIteration >= FromIteration then
   begin
+    var ThreadCount := Min(NThreads,FMaxThreads);
     FirstException := nil;
     TMonitor.Enter(Self);
     try
       LoopCompleted.ResetEvent;
       Next := FromIteration;
       IterationCount := ToIteration-FromIteration+1;
-      while (IterationCount > 0) and (ThreadCount < NThreads) do
+      while (IterationCount > 0) and (ActiveThreads < ThreadCount) do
       begin
-        var Thread := TIterationThread.Create(true);
-        Thread.Thread := ThreadCount;
-        Thread.Iteration := Next;
-        Thread.LoopIteration := Iteration;
-        Thread.Loop := Self;
-        Thread.FreeOnTerminate := true;
-        Thread.Start;
-        Inc(ThreadCount);
-        Inc(Next);
-        Dec(IterationCount);
+        ThreadPool[ActiveThreads].Stride := Stride;
+        ThreadPool[ActiveThreads].Iteration := Next;
+        ThreadPool[ActiveThreads].LoopIteration := Iteration;
+        ThreadPool[ActiveThreads].Active.SetEvent;
+        Inc(ActiveThreads);
+        Inc(Next,Stride);
+        Dec(IterationCount,Stride);
       end;
     finally
       TMonitor.Exit(Self);
@@ -132,6 +193,11 @@ end;
 
 Destructor TParallelFor.Destroy;
 begin
+  for var Thread := low(ThreadPool) to high(ThreadPool) do
+  begin
+    ThreadPool[Thread].Terminate;
+    ThreadPool[Thread].Active.SetEvent;
+  end;
   LoopCompleted.Free;
   inherited Destroy;
 end;
